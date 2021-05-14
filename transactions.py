49782c7684eb06a67bcc15693755069467647e0f
Azat-1997 <armingaleev97@yandex.ru>
2021-05-14 13:40:51 +0300

    Fixing transactions.py

--- transactions.py
+++ transactions.py
@@ -6,32 +6,40 @@ connection = sqlite3.connect("transaction.db")
 cursor = connection.cursor()
 # create table of users if this one not exists
  
-cursor.execute(""" CREATE TABLE IF NOT EXISTS users (id INT PRIMARY KEY, firstname TEXT NOT NULL, lastname TEXT NOT NULL, email TEXT); """)
+cursor.execute(""" CREATE TABLE IF NOT EXISTS users (userID INT PRIMARY KEY,
+ firstname TEXT NOT NULL,
+ lastname TEXT NOT NULL,
+ email TEXT); """)
  
-cursor.execute(""" CREATE TABLE IF NOT EXISTS trans (id INT PRIMARY KEY, user_id INT, amount REAL NOT NULL, date TEXT NOT NULL); """)
+cursor.execute(""" CREATE TABLE IF NOT EXISTS trans (transID INT PRIMARY KEY,
+ amount REAL,
+ date INT,
+ userID INT NOT NULL,
+ FOREIGN KEY (userID) REFERENCES users(userID)); """)
+
 connection.commit()
  
 
-def add_user(connection, id:int, firstname:str, lastname:str, email:str):
+def add_user(connection, firstname:str, lastname:str, email:str):
     cursor = connection.cursor()
-    cursor.execute("INSERT INTO users VALUES (?, ?, ?, ?);", (id, firstname, lastname, email))
+    cursor.execute("INSERT INTO users (firstname, lastname, email) VALUES (?, ?, ?);", (firstname, lastname, email))
     connection.commit()
  
         
-def add_transaction(connection, id, user_id: int, amount:str, date:str):
+def add_transaction(connection, user_id: int, amount:str, date:str):
     cursor = connection.cursor()
-    cursor.execute("INSERT INTO trans VALUES (?, ?, ?, ?);", (id, user_id, amount, date))
+    cursor.execute("INSERT INTO trans (user_id, amount, date) VALUES (?, ?, ?);", (user_id, amount, date))
     connection.commit()
     
 def update_user(connection, id:int, firstname:str, lastname, email):
     cursor = connection.cursor()
-    cursor.execute("UPDATE users SET firstname = ?, lastname = ?, email = ?, WHERE id = ? ;", (id, firstname, lastname, email))
+    cursor.execute("UPDATE users SET firstname = ?, lastname = ?, email = ?, WHERE userID = ? ;", (firstname, lastname, email, id))
     connection.commit()   
 
 
 def update_transaction(connection, id, user_id, amount, date):
     cursor = connection.cursor()
-    cursor.execute("UPDATE users SET user_id = ?, amount = ?, date = ?, WHERE id = ? ;", (id, user_id, amount, date))
+    cursor.execute("UPDATE users SET userID = ?, amount = ?, date = ?, WHERE transID = ?;", (user_id, amount, date, id))
     connection.commit()
 
     
@@ -47,3 +55,5 @@ def remove_transaction(connection, id):
 
 
 
+
+add_user(connection, )
